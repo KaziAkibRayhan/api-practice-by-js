2bfeee3fbe32767b458aeb232a5e741f6988a5d0
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-10-12 21:15:03 +0600

    completed meal db food api

--- js/mealdb.js
+++ js/mealdb.js
@@ -7,8 +7,8 @@ const loadMeal = (search) => {
 
 const displayFood = (meals) => {
     const mealContainer = document.getElementById('meal-container');
+    mealContainer.innerHTML = '';
     meals.forEach(meal => {
-        // console.log(meal);
         const mealDiv = document.createElement('div');
         mealDiv.classList.add('col-md-4');
         mealDiv.innerHTML = `
@@ -35,22 +35,39 @@ const loadMealDetails = (meal) => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => displayMealDetails(data))
-}
-
+        .then(data => displayMealDetails(data.meals[0]))
+};
 
 const displayMealDetails = (meal) => {
-    console.log(meal)
-}
-
-
+    const displayMealContainer = document.getElementById('display-detail-container');
+    displayMealContainer.innerHTML = '';
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('card');
+    cardDiv.style.width = '50rem';
+    cardDiv.innerHTML = `
+    <div class="card mb-3">
+            <div class="row g-0">
+                <div class="col-md-7">
+                    <img src="${meal.strMealThumb}" class="img-fluid h-100" alt="...">
+                </div>
+                <div class="col-md-5">
+                    <div class="card-body">
+                        <h5 class="card-title">${meal.strMeal}</h5>
+                        <p class="card-text">${meal.strInstructions.slice(0, 500)}...</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+    displayMealContainer.appendChild(cardDiv);
+};
 
 const searchFood = () => {
     const foodField = document.getElementById('food-field');
     const fooText = foodField.value;
     loadMeal(fooText);
     foodField.value = '';
-}
+};
 
 
 loadMeal('');
